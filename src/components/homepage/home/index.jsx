29972bd65498395a8c2d734e75a83cf4692c9bda
Abiola <ridwanabiola2000@gmail.com>
2024-10-15 10:51:04 +0100

    nothing exactly

--- src/components/homepage/home/index.jsx
+++ src/components/homepage/home/index.jsx
@@ -1,14 +1,14 @@
 const Home = () => {
   return (
   <>
-      <div className=' primary h-96 mydiv'>
+      <div className='primary h-96 mydiv'>
         <h2 className="text-center text-gray-400 text-7xl pt-16 font-semibold">Event Planning Is <br /> Hard.</h2>
         <p className="text-center text-2xl text-gray-400 font-bold pt-10">WE CAN HELP YOU</p>
     </div>
     
     <div className="bg-gray-500 h-screen text-black font-light text-center text-4xl">
-        <p className="p-5">
-          <span className="text-pink-900 font-medium">Tech Event</span> is a full service event management firm based in Calgary, Alberta that was created by pairing together our passion for business and events. We bring a fresh, unique approach to the event management industry.
+        <p className="p-5 pt-10">
+          <span className="text-pink-900 text-5xl font-medium">Tech Event</span> is a full service event management firm based in Calgary, Alberta that was created by pairing together our passion for business and events. We bring a fresh, unique approach to the event management industry.
         </p>
         
         <p className="p-10">
@@ -16,6 +16,10 @@ const Home = () => {
         or used to build networks and client loyalty.
         </p>
         
+    </div>
+
+    <div>
+      
     </div>
   </>
 
