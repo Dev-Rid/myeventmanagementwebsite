7fe5a42fe6235dc74cad14c18b2e40dd8d5a9614
Abiola <ridwanabiola2000@gmail.com>
2024-10-15 16:39:06 +0100

    errorfile component created with it's jsx and touch of tailwindcss styles

--- src/errorpage/index.jsx
+++ src/errorpage/index.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 
 const ErrorPage = () => {
     return (
-        <div className="text-center my-3">
-            <h2>Page not found</h2>
-            <p>Error 404</p>
-            <p>Go to <Link to="/">Homepage</Link></p>
+        <div className="bg-gray-600 text-center h-screen mx-auto p-20">
+            <h2 className="text-4xl">Page not found</h2> 
+            <p className="text-2xl text-red-500">Error 404</p>
+            <p className="text-2xl underline">Go to <Link to="/">Homepage</Link></p>
         </div>
     )
 }
