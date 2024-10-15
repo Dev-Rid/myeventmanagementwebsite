7e276b16dc5ad3fdac1f2930088b2fef6bbd0363
Abiola <ridwanabiola2000@gmail.com>
2024-10-15 11:09:28 +0100

    image 2 added with text on it and tailwind and css styling together

--- src/components/homepage/home/index.jsx
+++ src/components/homepage/home/index.jsx
@@ -1,25 +1,29 @@
 const Home = () => {
   return (
   <>
-      <div className='primary h-96 mydiv'>
-        <h2 className="text-center text-gray-400 text-7xl pt-16 font-semibold">Event Planning Is <br /> Hard.</h2>
-        <p className="text-center text-2xl text-gray-400 font-bold pt-10">WE CAN HELP YOU</p>
-    </div>
-    
-    <div className="bg-gray-500 h-screen text-black font-light text-center text-4xl">
-        <p className="p-5 pt-10">
-          <span className="text-pink-900 text-5xl font-medium">Tech Event</span> is a full service event management firm based in Calgary, Alberta that was created by pairing together our passion for business and events. We bring a fresh, unique approach to the event management industry.
-        </p>
-        
-        <p className="p-10">
-        Our team understands that a properly executed event can be leveraged to support an organization’s strategic vision, incorporated into a company’s marketing plan,
-        or used to build networks and client loyalty.
-        </p>
-        
-    </div>
-
-    <div>
+      <div className="h-dvh bg-gray-500">
+      <div className='primary h-[30rem] mydiv pt-5'>
+          <h2 className="text-center text-gray-400 text-7xl pt-16 font-semibold">Event Planning Is <br /> Hard.</h2>
+          <p className="text-center text-2xl text-gray-400 font-bold pt-10">WE CAN HELP YOU</p>
+      </div>
       
+      <div className="p-9 bg-gray-500 h-[35rem] text-black font-light text-center text-4xl">
+          <p className=" pt-10">
+            <span className="text-pink-900 text-5xl font-medium">Tech Event</span> is a full service event management firm based in Calgary, Alberta that was created by pairing together our passion for business and events. We bring a fresh, unique approach to the event management industry.
+          </p>
+          
+          <p className="p-10">
+          Our team understands that a properly executed event can be leveraged to support an organization’s strategic vision, incorporated into a company’s marketing plan,
+          or used to build networks and client loyalty.
+          </p>
+          
+      </div>
+
+    
+      <div className="primary2 pt-20 h-[20rem] text-xl text-gray-400 text-center">
+        <h2 className="text-8xl">Event Planing</h2>
+          <p className="text-4xl pt-3"> Is Our Passion</p>
+      </div>
     </div>
   </>
 
