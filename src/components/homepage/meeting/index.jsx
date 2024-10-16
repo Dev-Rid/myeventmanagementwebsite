8ec6480722fbc2772b124d7d8c269e68f2644f2e
Abiola <ridwanabiola2000@gmail.com>
2024-10-16 15:33:49 +0100

    my meeting component content added, styling coming...

--- src/components/homepage/meeting/index.jsx
+++ src/components/homepage/meeting/index.jsx
@@ -2,8 +2,139 @@ import React from 'react'
 
 const Meeting = () => {
   return (
-    <div>
-      <h2>meeting</h2>
+    <div className='bg-gray-500'>
+      <div>
+        <div>
+          <h2>Busy, Busy, Busy</h2>
+          <p>A few of our recent events</p>
+        </div>
+
+      <div>
+          <div>
+            <div>
+              <h3>Spark Event Management</h3>
+              <span>MAIL: <p>Unit #72148 - 1600 90 Avenue SW, Calgary, AB T2V 5H9</p> </span>
+              <span>OFFICE: <p>Unit #200 - 1110 7 Street SW, Calgary, AB T2R 1</p> </span>
+              <span>o. (587) 880-2672   m. (403) 990-8968</span>
+              <span>tf. 1-800-281-0697   f. 1-800-281-0697</span>
+            </div>
+
+            <div>
+              <h2>api</h2>
+            </div>
+
+            <div>
+              <h2>Name <span>(required)</span></h2>
+              
+              <div>
+                <div>
+                  <label htmlFor="">First Name</label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <label htmlFor="">Last Name</label>
+                  <input type="text" />
+                </div>
+              </div>
+
+              <div>
+                <label htmlFor="">Email Address <span>(required)</span></label>
+                <input type="text" />
+
+
+                <label htmlFor="">Phone</label>
+                <input type="text" />
+
+
+                <label htmlFor="">Subject</label>
+                <input type="text" />
+
+
+                <label htmlFor="">Message <span>(required)</span></label>
+                <input type="text" />         
+
+                <button>SAY HI!</button>
+              </div>
+            </div>
+          </div>
+         
+          <div>
+            <div>
+              <span>
+                <img src="" alt="" />
+                <p>As a female-owned business, 
+                  we owe our success to the amazing 
+                  women who have shared their talent
+                  with Spark.…
+                </p>
+                <a href="">
+                  https://t.co/PakmHWuN9s
+                  Mar 8, 2022, 8:00 AM
+                </a>
+              </span>
+
+              <span>
+                <img src="" alt="" />
+                <p>
+                  All that love you're looking for? 
+                  Give it to yourself. Happy ❤️ day, 
+                  besties.
+                </p>
+                <a href="">
+                  https://t.co/FiAMw7Qt2z
+                  Feb 14, 2022, 8:01 AM
+                </a>
+              </span>
+
+              <span>
+                <img src="" alt="" />
+                <p>
+                  This is how we're sleeping tonight 
+                  knowing we aren't planning the biggest 
+                  sporting event of the year... Good luck t…
+                </p>
+                <a href="">
+                  https://t.co/kztLXV1RmW
+                  Feb 12, 2022, 9:05 AM
+                </a>
+              </span>
+
+              <span>
+                <img src="" alt="" />
+                <p>
+                  #TBT: How cute were these sweet treats we
+                  gave for Valentines Day? To sweeten the deal
+                  even more, we had limited-ti…
+                </p>
+                <a href="">
+                  https://t.co/fTFPnqNJIK
+                  Feb 10, 2022, 9:01 AM
+                </a>
+              </span>
+
+              <span>
+                <img src="" alt="" />
+                <p>
+                  We hope everyone had a wonderful holiday break 
+                  and were able to cherish the quiet moments of 
+                  the season. Looking fo…
+                </p>
+                <a href="">
+                  https://t.co/uyze1eZjzP
+                  Jan 3, 2022, 7:01 AM
+                </a>
+              </span>
+              
+              <span>
+                twitter
+              </span>
+            </div>
+
+          </div>
+      </div>
+      </div>
+
+
     </div>
   )
 }
