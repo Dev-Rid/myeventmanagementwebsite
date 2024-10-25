c60fe4cb649a7bed785d53e66417c20107f489f0
Abiola <ridwanabiola2000@gmail.com>
2024-10-25 16:40:38 +0100

    nothing in particular

--- src/components/homepage/blog/hook/index.jsx
+++ src/components/homepage/blog/hook/index.jsx
@@ -6,95 +6,107 @@ export const blogContent = () =>{
             body: "Do you like things to be organized and orderly? Do you thrive using post-its and to-do lists? Is colour-coding your past time of choice? If so, we might have the job for you! Spark Event Management(Spark) is seeking a contract event & administrative assistant to join our team in Calgary, AB.We ask that the candidate be available for 30 - 40 hours per week for this position, starting immediately.The ideal candidate for this position is a highly organized, detail - oriented and self - motivated individual, who thrives in a dynamic and entrepreneurial environment.",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Jul 8 2019"
+            date: "Jul 8, 2019",
+            id: 1
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/5+reasons+why.jpg",
             title: "5 Benefits of Attending a Conference",
             body: "Webinars and online training tools are replacing the idea of conferences and professional development events. That’s what they are saying, but we disagree! There is unique power in being connected to other people who are active in your line of work, through education and networking. I believe that professional development is best experienced in person and here's why.",
             more: "Read More →",
             writer: "Written by Kim",
-            date: "Aug 10, 2018"
+            date: "Aug 10, 2018",
+            id: 2
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Earlybird-01.jpg",
             title: "The Earlybird Catches the Worm But...",
             body: "'Should we extend the earlybird deadline?' We get asked this all the time. And I'm sorry, but the answer is, 'No!' If you are finding that your marketing tactics are not drawing in registrations as fast as you hoped before the earlybird deadline, don't panic. We have some tried and true tactics that can get your registrations to that sold out scenario.",
             more: "Read More →",
             writer: "Written by Kim",
-            date: "Aug 10, 2018"
+            date: "Aug 10, 2018",
+            id: 3
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Spark_Blog_Power+of+Branding.jpg",
             title: "The Power of Branding",
             body: "Just pick a type-face, slap on a bold colour, and presto you're done. Wrong. Branding is the face to your event - your big first impression. If your event branding is professional and polished, you’re also communicating that all aspects of the event will have the same attention to detail.",
             more: "Read More →",
             writer: "Written by erinn",
-            date: "May 1, 2018"
+            date: "May 1, 2018",
+            id: 4
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Green+is+the+new+black-01.jpg",
             title: "Green is the New Black",
             body: "Throughout 2017, headlines about climate change agreements, carbon taxes, pending pipeline deals, emissions, natural disasters, melting ice caps and unpredictable weather patterns have dominated our headlines. As meeting and event leaders, a big part of our job is to engage and participate in conversations about how our events are impacting the environment.",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Apr 8, 2018"
+            date: "Apr 8, 2018",
+            id: 5
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Survival+Skills-01.jpg",
             title: "Six Spring Slaughter Survival Skills",
             body: "If you have been living under a rock and have not seen our #SparkBTS this past June, well let me fill you in. Spring is by far the busiest season for us at Spark. In true form, we like to affectionately call this Spring Slaughter. Spring Slaughter is a marathon, not a sprint. And a part of crossing the finish line, like any marathon, requires prep, pacing, and a few tricks of the trade…",
             more: "Read More →",
             writer: "Written by Kim",
-            date: "Jun 30, 2016"
+            date: "Jun 30, 2016",
+            id: 6
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Client+Spotlight-01.jpg",
             title: "Managing With a Fresh Perspective at PMINAC",
             body: "There was no shortage of vibrancy and excitement in May at the Project Management Institute Northern Alberta Chapter (PMINAC) Conference. With updated branding, a new spacious venue, and a refreshed vision for a conference experience, the Spark team partnered with one of our long-time clients to make months of creative rebranding come to life.",
             more: "Read More →",
             writer: "Written by Kim",
-            date: "Jul 31, 2015"
+            date: "Jul 31, 2015",
+            id: 7
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Repeat-01.jpg",
             title: "Repeat Clients... Getting To Know You!",
             body: "Anyone who lives or works in Calgary knows that during Stampede, the city morphs into a massive 10 day, cowboy-themed party. Venues, event planners, décor companies and entertainers are all booked up working long days and late hours to satisfy the demands of one of the busiest party seasons in our city. Most Stampede events and parties offer little to challenge the imagination of an event planner…",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Sep 25, 2013"
+            date: "Sep 25, 2013",
+            id: 8
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/BOMA+Awards+Dinner-01.jpg",
             title: "BOMA Calgary Excellence Awards Dinner",
             body: "Last year, I attended the National CSAE Conference and had the pleasure of meeting Lia Robinson and William (Bill) Partridge from the Building Owners and Managers Association (BOMA) Calgary office. It was immediately evident to me how passionate and dedicated both Lia and Bill were to their membership and their organization.",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Jul 25, 2013"
+            date: "Jul 25, 2013",
+            id: 9
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Doing+Our+Part_Blog-01.jpg",
             title: "Doing Our Part... Western Canada In-School Settlement Conference",
             body: "Last month, we had the honour of working with the Calgary Bridge Foundation for Youth in partnership with Citizenship and Immigration Canada to put on the Western Canada In-School Settlement Conference. This conference, the first of its kind, brought together school based settlement programs and 200 in-school settlement workers from across Western Canada to develop practical skills and tools, discuss common challenges and create innovative program ideas. ",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Mar 4, 2013"
+            date: "Mar 4, 2013",
+            id: 10
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Started+with+Fries_Blog-01.jpg",
             title: "Would You Like Fries With That?",
             body: "It’s been almost 6 months since I fully launched Spark and ventured into the world of entrepreneurship on my own. Over the last 6 months, I have had a lot of family and friends ask questions like “is Spark still in business?”, “are things going okay?”, “do you have any clients?”, “Can you afford to pay the bills?” Fortunately, I get to answer “yes” to all of these questions…",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Jan 23, 2013"
+            date: "Jan 23, 2013",
+            id: 11
         },
         {
-            image: "/src/IMGs",
+            image: "/src/IMGs/Spark_Blog_Power+of+Branding.jpg",
             title: "And a Spark is Lit...",
             body: "When people ask me what I do for a living, I say I own an event management firm. 80% of the time, it leads to discussion of weddings, confetti and the latest trends in balloon animals, but I’m used to it. You see, it does not matter what people call me or what they imagine my job is like day to day, give me 5 minutes with them and they’ll never again utter the words “party planner”.  I know events can include things like confetti and balloon animals, but they are so much more than that…",
             more: "Read More →",
             writer: "Written by Anh",
-            date: "Oct 29, 2012"
+            date: "Oct 29, 2012",
+            id: 12
         },
 
     ]
--- src/components/homepage/blog/index.jsx
+++ src/components/homepage/blog/index.jsx
@@ -2,206 +2,67 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { blogContent } from './hook'
 
 const Blog = () => {
+
+  const { blogPreContent } = blogContent()
+  
   return (
     <div className="bg-gray-500">
       <div className='blogBg h-[18rem] bg-cover text-center pt-20 opacity-30'>
         <h2 className='text-8xl font-semilight text-gray-400'>Thoughts & News</h2>
       </div>
 
+    <div className='grid grid-cols-4'>
+      <div className='col-span-3'>
+          {(blogPreContent.map((eachBlog) => (
+            <div className=''>
+              <div key={eachBlog.id} className='flex px-20 py-16'>
+                <div>
+                  <img src={eachBlog.image} alt="" className='cursor-pointer h-[9rem] w-[15rem] rounded-sm my-2' />
+                </div>
+                <div className='w-[50%] mx-5'>
+                  <h1 className='text-2xl'>{eachBlog.title}</h1>
+                  <p className='py-1 font-semilight'>{eachBlog.body}</p>
+                  <a href="" className='font-light'>{eachBlog.more}</a>
+                  <div className='flex font-light py-3'>
+                    <p className='pr-3'>{eachBlog.writer}</p> •
+                    <p className='mx-2'>{eachBlog.date}</p>
+                  </div>
+                </div>
+              </div>
 
-      { }
-      {/* <div>
-        <div>
-          <div>
-            <img src="" alt="" />
-            <h2>Do You Like to Colour Code!?</h2>
-            <p>
-              Do you like things to be organized and orderly? Do you thrive 
-              using post-its and to-do lists? Is colour-coding your past time of 
-              choice? If so, we might have the job for you! Spark Event Management 
-              (Spark) is seeking a contract event & administrative assistant to join 
-              our team in Calgary, AB. We ask that the candidate be available for 30-40 
-              hours per week for this position, starting immediately. The ideal 
-              candidate for this position is a highly organized, detail-oriented 
-              and self-motivated individual, who thrives in a dynamic and 
-              entrepreneurial environment.
-            </p>
-
-            <div>
-              <span>
-                <Link>Read More ⇾</Link>
-              </span>
-              <span>
-                <p>Written by Anh</p>
-                <p>Jul 8, 2019</p>
-              </span>
             </div>
-          </div>
 
-          <div>
-            <input type="text" placeholder='Search'/>
-
-            <div>
-              <label htmlFor="options">Archive</label>
-              <select id="options" name="options">
-                <option value="option1">Option 1</option>
-                <option value="option2">Option 2</option>
-                <option value="option3">Option 3</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <img src="" alt="" />
-          <h2>5 Benefits of Attending a Conference</h2>
-          <p>
-            Webinars and online training tools are replacing the 
-            idea of conferences and professional development events. 
-            That’s what they are saying, but we disagree! There is 
-            unique power in being connected to other people who are 
-            active in your line of work, through education and networking. 
-            I believe that professional development is best experienced in 
-            person and here's why.
-          </p>
-
-          <div>
-            <span>
-              <Link>Read More ⇾</Link>
-            </span>
-            <span>
-              <p>Written by Kim</p>
-              <p> Aug 10, 2018</p>
-            </span>
-          </div>
-        </div>
-
-        <div>
-          <img src="" alt="" />
-          <h2>The Earlybird Catches the Worm But...</h2>
-          <p>
-            "Should we extend the earlybird deadline?" 
-            We get asked this all the time. And I'm sorry, but the answer is, 
-            "No!" If you are finding that your marketing tactics are not drawing 
-            in registrations as fast as you hoped before the earlybird deadline, 
-            don't panic. We have some tried and true tactics that can get your
-            registrations to that sold out scenario.
-          </p>
-
-          <div>
-            <span>
-              <Link>Read More ⇾</Link>
-            </span>
-            <span>
-              <p>Written by Kim</p>
-              <p> Aug 10, 2018</p>
-            </span>
-          </div>
-        </div>
-
-
-        <div>
-          <img src="" alt="" />
-          <h2>Green is the New Black</h2>
-          <p>
-            Throughout 2017, headlines about climate change agreements,
-             carbon taxes, pending pipeline deals, emissions, natural disasters,
-              melting ice caps and unpredictable weather patterns have dominated 
-              our headlines. As meeting and event leaders, a big part of our job
-              is to engage and participate in conversations about how our events
-              are impacting the environment.
-          </p>
-
-          <div>
-            <span>
-              <Link>Read More ⇾</Link>
-            </span>
-            <span>
-              <p>Written by Anh</p>
-              <p> Apr 9, 2018</p>
-            </span>
-          </div>
-        </div>
-
-
-      <div>
-        <img src="" alt="" />
-          <h2>Six Spring Slaughter Survival Skills</h2>
-        <p>
-            If you have been living under a rock and have not seen our 
-            #SparkBTS this past June, well let me fill you in. Spring is 
-            by far the busiest season for us at Spark. In true form, we 
-            like to affectionately call this Spring Slaughter. Spring 
-            Slaughter is a marathon, not a sprint. And a part of crossing 
-            the finish line, like any marathon, requires prep, pacing, and
-             a few tricks of the trade…
-        </p>
-
-        <div>
-          <span>
-            <Link>Read More ⇾</Link>
-          </span>
-          <span>
-            <p>Written by Kim</p>
-            <p>Jun 30, 2016</p>
-          </span>
-        </div>
+          )))}
       </div>
+      <div className='col-span-1 py-16'>
+          <input type="text" placeholder='Search' className='h-[3.5rem] w-[15rem] rounded-sm text-center font-xl bg-gray-300' />
+
+          <div className='py-5 '>
+            {/* <label htmlFor="options">Archive</label> */}
+            <select id="options" name="options" className='h-[3rem] w-[15rem] rounded-sm text-center bg-gray-300'>
+              <option value="option1">Archive</option>
+              <option value="option2">Jul 8, 2019</option>
+              <option value="option3">Aug 10, 2018</option>
+              <option value="option4">May 1, 2018</option>
+              <option value="option5">Jul 25, 2013</option>
+              <option value="option6">Mar 4, 2013</option>
+              <option value="option7">Apr 8, 2018</option>
+              <option value="option8">Jun 30, 2016</option>
+              <option value="option9">Sep 25, 2013</option>
+              <option value="option10">Oct 29, 2012</option>
+              <option value="option11">Jul 31, 2015</option>
+              <option value="option">Jan 23, 2013</option>
 
 
-        <div>
-          <img src="" alt="" />
-          <h2>Managing With a Fresh Perspective at PMINAC</h2>
-          <p>
-            There was no shortage of vibrancy and excitement in May at the 
-            Project Management Institute Northern Alberta Chapter (PMINAC)
-            Conference. With updated branding, a new spacious venue, and a
-            refreshed vision for a conference experience, the Spark team
-            partnered with one of our long-time clients to make months of
-            creative rebranding come to life.
-          </p>
-
-          <div>
-            <span>
-              <Link>Read More ⇾</Link>
-            </span>
-            <span>
-              <p>Written by Kim</p>
-              <p>Jul 31, 2015</p>
-            </span>
+            </select>
           </div>
         </div>
-
-
-        <div>
-          <img src="" alt="" />
-          <h2>And a Spark is Lit...</h2>
-          <p>
-            When people ask me what I do for a living, I say I own an event management firm.
-            80% of the time, it leads to discussion of weddings, confetti and the 
-            latest trends in balloon animals, but I’m used to it. You see, it does
-            not matter what people call me or what they imagine my job is like day 
-            to day, give me 5 minutes with them and they’ll never again utter the
-            words “party planner”.  I know events can include things like confetti 
-            and balloon animals, but they are so much more than that…
-          </p>
-
-          <div>
-            <span>
-              <Link>Read More ⇾</Link>
-            </span>
-            <span>
-              <p>Written by Anh</p>
-              <p>Oct 29, 2012</p>
-            </span>
-          </div>
-        </div>
-
-      </div> */}
-
-
+    </div>
+      
+ 
 
       <div className='flex justify-center p-20 h-54 my-10'>
         <Link className='text-3xl'>
